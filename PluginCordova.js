23d50c05d7e7aba3bf9d08a3907c354813a80fcd
nitroni <nitroni45@gmail.com>
2014-09-09 15:23:16 -0500

    actualizacion2
    dfdf

--- PluginCordova.js
+++
@@ -1,9 +0,0 @@
-window.echo = function(str, callback) {
-  cordova.exec(
-    callback,
-    function(err) { callback('Nothing to echo.'); },
-    "MyPhonegapInterface",
-    "echo",
-    [str]
-  );
-};
