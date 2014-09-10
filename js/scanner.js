3548c6c7af00a065b339b1fbd25116da34bcf0ef
nitroni <nitroni45@gmail.com>
2014-09-10 09:17:08 -0500

    actualizacion
    12121

--- js/scanner.js
+++ js/scanner.js
@@ -4,26 +4,8 @@ scanner.scan(
     function (result) 
 	{
 		hideAllPages();
-		alert("dato=="+result.text);
-	    if(result.text == "page2")
-	    {
-	        document.getElementById('page2').style.visibility = 'visible';
-	        window.location.hash="#page2";
-	    }
-	    else if(result.text == "page3")
-	    {
-	        document.getElementById('page3').style.visibility = 'visible';			
-	        window.location.hash="#page3";
-	    }
-	    else if(result.text == "page4")
-	    {
-	        document.getElementById('page4').style.visibility = 'visible';			
-	        window.location.hash="#page4";
-	    }
-		else
-		{
-            alert("Not a valid QR Code for this application="+result);
-		}
+		alert("dato=="+result);
+	    
     }, 
         function (error) 
 	    {
@@ -35,8 +17,8 @@ scanner.scan(
 /* Helper for this example that hides all four pages */
 function hideAllPages()
 {
-	document.getElementById('page1').style.visibility = 'hidden';
-	document.getElementById('page2').style.visibility = 'hidden';
-	document.getElementById('page3').style.visibility = 'hidden';		
-	document.getElementById('page4').style.visibility = 'hidden';
+	//document.getElementById('page1').style.visibility = 'hidden';
+	//document.getElementById('page2').style.visibility = 'hidden';
+	//document.getElementById('page3').style.visibility = 'hidden';		
+	//document.getElementById('page4').style.visibility = 'hidden';
 }
