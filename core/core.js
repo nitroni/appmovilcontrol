d6334615858928fc36425af0ca41be2b2d8cd94d
nitroni <nitroni45@gmail.com>
2014-09-12 12:39:44 -0500

    actualizacion11
    actualizacion11

--- core/core.js
+++ core/core.js
@@ -1,14 +1,18 @@
-var url='http://jsonp.jit.su/?url=http%3A%2F%2F181.48.24.156%3A8183%2FServicios%2Fapi%2FConsumidor%2FFilter%2F';
+//Core proveedor
+var url='http://jsonp.jit.su/?url=http%3A%2F%2F181.48.24.156%3A8183%2FServicios%2Fapi%2FProveedor%2FFilter%2F%3Fid%3D';
 function ValidarLogin() {
+
 	var datosUsuario = $("#nombredeusuario").val();
 	var datosPassword = $("#clave").val();	
 	var NameServicio="";
 	$.ajax({ // ajax call starts
-          url: url+datosUsuario+'%2F'+datosPassword+'', // JQuery loads serverside.php 
+          url: url+datosUsuario+'sx%26clave%3D'+datosPassword+'', // JQuery loads serverside.php 
+		  
           dataType: 'json', // Choosing a JSON datatype		   
           success: function(data) // Variable data contains the data we get from serverside
           {
-			  if(data!=""){
+			  if(data.CelProveedor>0){
+			     $('#coreeventos').empty();
 				 $.mobile.changePage("#home");
                  ListarEventos(data);		
 			  }
@@ -23,11 +27,12 @@ function ValidarLogin() {
 }
 function ListarEventos(data){
  var i=0;
+ //Se limpia el contenido
 	 //Listamos los eventos que tiene esta persona disponibles
-	 while (i < data.Derechos.length){
-		 NameServicio=data.Derechos[i].DesServicio;
+	 while (i < data.Servicios.length){
+		 NameServicio=data.Servicios[i].DesServicio;
 		  if (NameServicio !== null) {
-			  $("#coreeventos").append('<li data-section="Widgets" data-filtertext="selectmenus custom native multiple optgroup disabled forms" data-corners="false" data-shadow="false" data-iconshadow="true" data-wrapperels="div" data-icon="false" data-iconpos="right" data-theme="d" class="ui-btn ui-btn-up-d ui-btn-icon-right ui-li-has-arrow ui-li"><div class="ui-btn-inner ui-li"><div class="ui-btn-text"><a href="#menucontrolscan" class="ui-link-inherit">'+NameServicio+'</a></div></div></li>');     
+			  $("#coreeventos").append('<li data-section="Widgets" data-filtertext="selectmenus custom native multiple optgroup disabled forms" data-corners="false" data-shadow="false" data-iconshadow="true" data-wrapperels="div" data-icon="false" data-iconpos="right" data-theme="d" class="ui-btn ui-btn-up-d ui-btn-icon-right ui-li-has-arrow ui-li"><div class="ui-btn-inner ui-li"><div class="ui-btn-text"><a href="#menucontrolscan" class="ui-link-inherit">'+NameServicio+'</a></div></div></li><br>');     
 		  }
 		 i=i+1;
    }
