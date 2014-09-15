6c4941d3334654ff168238daae97edb27f9da30a
nitroni <nitroni45@gmail.com>
2014-09-15 01:51:35 -0500

    ultima actualizacion full
    sdsdsd

--- core/core.js
+++ core/core.js
@@ -6,6 +6,13 @@ var datosPassword="";
 var codigoservicio="";
 var datosg;
 var tiposervice="";
+var estado='no';
+var nitproveedor="";
+var detfechainiserv="";
+var detfechafinserv="";
+var detcapaserv="";
+var capservicio="";
+var conteventotal="";
 function ValidarLogin() {
 datosUsuario = $("#nombredeusuario").val();
 datosPassword = $("#clave").val();	
@@ -38,22 +45,33 @@ function ListarEventos(data){
 	 while (i < data.Servicios.length){
 		 NameServicio=data.Servicios[i].DesServicio;
 		 codigoservicio=data.Servicios[i].CodServicio;
+		 nitproveedor=data.Servicios[i].NitProveedor;
+		 detfechainiserv=data.Servicios[i].RanIniDisServicio;
+         detfechafinserv=data.Servicios[i].RanFinDisServicio;
+         detcapaserv=data.Servicios[i].CapServicio;
 		 tiposervicio="c";
 		  if(NameServicio=="Restaurante"){
 		     tiposervicio="r";
 		  }
 		  if (NameServicio !== null) {
-			  $("#coreeventos").append('<li data-section="Widgets" data-filtertext="selectmenus custom native multiple optgroup disabled forms" data-corners="false" data-shadow="false" data-iconshadow="true" data-wrapperels="div" data-icon="false" data-iconpos="right" data-theme="d" class="ui-btn ui-btn-up-d ui-btn-icon-right ui-li-has-arrow ui-li"><div class="ui-btn-inner ui-li"><div class="ui-btn-text"><button id = "vcedu"  onclick="opcionservicio('+"'"+codigoservicio+"'"+',tiposervicio);" class="ui-btn-text">'+NameServicio+'</button></div></div></li><br>');     
+			  $("#coreeventos").append('<li data-section="Widgets" data-filtertext="selectmenus custom native multiple optgroup disabled forms" data-corners="false" data-shadow="false" data-iconshadow="true" data-wrapperels="div" data-icon="false" data-iconpos="right" data-theme="d" class="ui-btn ui-btn-up-d ui-btn-icon-right ui-li-has-arrow ui-li"><div class="ui-btn-inner ui-li"><div class="ui-btn-text"><button id = "vcedu"  onclick="opcionservicio('+"'"+codigoservicio+"'"+',tiposervicio,'+"'"+nitproveedor+"'"+','+"'"+detcapaserv+"'"+');" class="ui-btn-text">'+NameServicio+'</button></div><button id = "vcedu"  onclick="detalleservicio('+"'"+detfechainiserv+"'"+','+"'"+detfechafinserv+"'"+','+"'"+detcapaserv+"'"+');" >Detalle</button></div></li><br>');     
 		  }
 		 i=i+1;
    }
 }
-function opcionservicio(cdv,tip){   
+function opcionservicio(cdv,tip,nitpro,cap){   
      cdv.replace(' ','');
-	 alert("op="+cdv);
 	 tiposervice=cdv;
+	 nitproveedor=nitpro;
+	 capservicio=cap;
      cargarcontador(cdv,tip);
      $.mobile.changePage("#menucontrolscan");	 
+}
+function detalleservicio(fechainicio,fechafin,capacidad){
+	 $("#detalle1").text('Fecha de inicio del servicio: '+fechainicio+'');
+	 $("#detalle2").text('Fecha de finalización del servicio: '+fechafin+'');
+	 $("#detalle3").text('Capacidad: '+capacidad+'');
+	 $.mobile.changePage("#detalle");
 }
 function cargarcontador(cdv,tip) {
 var url='http://181.48.24.156:8183/Servicios/api/Proveedor/Filter/?id='+datosUsuario+'sx&clave='+datosPassword+'';	
@@ -74,9 +92,8 @@ var url='http://181.48.24.156:8183/Servicios/api/Proveedor/Filter/?id='+datosUsu
 		  }
       });
 }
-//muestra el contador de inicio
+//Muestra el contador de inicio
 function Mostrarcontador(datam,codiserv){
-alert("codigo servicioh="+tiposervice);
 	var i=0;
 	var total=0;
 	while (i < datam.Servicios.length){
@@ -86,18 +103,25 @@ alert("codigo servicioh="+tiposervice);
 	          $("#datoscontador1").text('Entradas: '+datam.Servicios[i].ConEntradas+'');
 			  $("#datoscontador2").text('Salidas: '+datam.Servicios[i].ConSalidas+'');
 			  $("#datoscontador3").text('Total evento: '+total+'');
+	          $("#datconuno1").text('Entradas: '+datam.Servicios[i].ConEntradas+'');
+	          $("#datconuno2").text('Salidas: '+datam.Servicios[i].ConSalidas+'');
+	          $("#datconuno3").text('Total evento: '+total+''); 
+              $("#contegresar1").text('Entradas: '+datam.Servicios[i].ConEntradas+'');
+	          $("#contegresar2").text('Salidas: '+datam.Servicios[i].ConSalidas+'');
+	          $("#contegresar3").text('Total evento: '+total+''); 	 
 		  }
 		 i=i+1;
     }           			  			  				
 }
 function validarqr(codigoqr) {
-//Se arma el objeto con los parametros a enviar
+var fecha = new Date();
+var fechaactual=(fecha.getMonth()+1)+'/'+fecha.getDate()+'/'+fecha.getFullYear()+' '+fecha.getHours()+':'+fecha.getMinutes()+':'+fecha.getSeconds();
 var datosent=
 {
-   "NitProveedor":999999999.0,
+   "NitProveedor":nitproveedor,
    "CedConsumidor":codigoqr,
    "CodServicio":tiposervice,
-   "FecEntrada":"02/02/2014 09:08:03",
+   "FecEntrada":fechaactual,
    "TipRegistro":"E",
    "Message":null,
    "ConRegistro":1,
@@ -105,7 +129,6 @@ var datosent=
 }
 //Se define la url del servicio
 var url='http://181.48.24.156:8183/Servicios/api/Registro/Add';	
-var estado='no';
 	$.ajax({ // ajax call starts
           url: url, // JQuery loads serverside
 		  type:"POST",
@@ -119,9 +142,18 @@ var estado='no';
           data: JSON.stringify(datosent),		  
           success: function(data) // Variable data contains the data we get from serverside
           {   
-		      $('.cont').empty();
-              updatecounter(data.ConEntradasServicio,data.ConSalidasServicio);	
-			  estado='si';
+		      conteventotal=(data.ConEntradasServicio-data.ConSalidasServicio);
+			  if(conteventotal<0){
+			     conteventotal=0;
+			  }
+			  if(conteventotal<=capservicio){
+				  $('.cont').empty();
+				  updatecounter(data.ConEntradasServicio,data.ConSalidasServicio);	
+				  estado='si';
+			  }
+			  if(conteventotal>capservicio){
+			     capacidadevento(capservicio);
+			  }
           },
 		  error: function(data){
 		       //alert("Error en la conexion: la cedula no es valida");
@@ -138,11 +170,31 @@ function updatecounter(entrada,salida){
 	   }
 	   $("#datoscontador1").text('Entradas: '+entrada+'');
 	   $("#datoscontador2").text('Salidas: '+salida+'');
-	   $("#datoscontador3").text('Total evento: '+numpersonevento+'');      
+	   $("#datoscontador3").text('Total evento: '+numpersonevento+''); 
+       $("#datconuno1").text('Entradas: '+entrada+'');
+	   $("#datconuno2").text('Salidas: '+salida+'');
+	   $("#datconuno3").text('Total evento: '+numpersonevento+''); 	
+	   $("#contegresar1").text('Entradas: '+entrada+'');
+	   $("#contegresar2").text('Salidas: '+salida+'');
+	   $("#contegresar3").text('Total evento: '+numpersonevento+''); 	
 }
 function validarcedula(){
-   var url='http://181.48.24.156:8183/Servicios/api/Registro/Add';	
-   var cedula=document.getElementById("cedula").value;
+    var cedula=document.getElementById("numcedula").value;
+	var fecha = new Date();
+    var fechaactual=(fecha.getMonth()+1)+'/'+fecha.getDate()+'/'+fecha.getFullYear()+' '+fecha.getHours()+':'+fecha.getMinutes()+':'+fecha.getSeconds();
+	//Se arma el objeto con los parametros a enviar
+	var datosentc=
+	{
+	   "NitProveedor":nitproveedor,
+	   "CedConsumidor":cedula,
+	   "CodServicio":tiposervice,
+	   "FecEntrada":fechaactual,
+	   "TipRegistro":"E",
+	   "Message":null,
+	   "ConRegistro":1,
+	   "CodProducto":""
+	}
+   var url='http://181.48.24.156:8183/Servicios/api/Registro/Add';	  
    $.ajax({ // ajax call starts
           url: url, // JQuery loads serverside
 		  type:"POST",
@@ -153,36 +205,120 @@ function validarcedula(){
           dataType: "json",
 		  async: false,
           crossDomain: true,
-          data: JSON.stringify(datosent),			  
+          data: JSON.stringify(datosentc),			  
           success: function(data) // Variable data contains the data we get from serverside
           {   
-		      $('#datoscontador').empty();
-              Mostrarcontador(data,cdv);	
-			  alert("La cedula "+cedula+" es valida");
-			  document.getElementById("numcedula").value="";
+		      conteventotal=(data.ConEntradasServicio-data.ConSalidasServicio);
+			  if(conteventotal<0){
+			      conteventotal=0;
+			  }
+			  if(conteventotal<=capservicio){
+				  $('.cont').empty();
+				  updatecounter(data.ConEntradasServicio,data.ConSalidasServicio);
+				  estado='si';
+				  alert("La cédula es valida");
+			  }
+			  if(conteventotal>capservicio){
+			      capacidadevento(capservicio);
+			  }			  
           },
 		  error: function(data){
-		       alert("Error en la conexion");
+			  alert("La cédula "+cedula+" no es valida");
 		  }
       });
 }
 function validaranonimo(){
+    var fecha = new Date();
+    var fechaactual=(fecha.getMonth()+1)+'/'+fecha.getDate()+'/'+fecha.getFullYear()+' '+fecha.getHours()+':'+fecha.getMinutes()+':'+fecha.getSeconds();
+    var datosenta=
+	{
+	   "NitProveedor":nitproveedor,
+	   "CedConsumidor":713999,
+	   "CodServicio":tiposervice,
+	   "FecEntrada":fechaactual,
+	   "TipRegistro":"E",
+	   "Message":null,
+	   "ConRegistro":1,
+	   "CodProducto":""
+	}
    var url='http://181.48.24.156:8183/Servicios/api/Registro/Add';	
    $.ajax({ // ajax call starts
-          url: url, // JQuery loads serverside.php 
+          url: url, // JQuery loads serverside
 		  type:"POST",
-          dataType: 'json', // Choosing a JSON datatype	
-		  timeout: 5000,	
+		  headers: { 
+				 Accept : "text/plain; charset=utf-8",
+				"Content-Type": "text/plain; charset=utf-8"
+          },
+          dataType: "json",
+		  async: false,
           crossDomain: true,
-          data: '{"NitProveedor": 999999999.0,"CedConsumidor": 0,"CodServicio": "SRV001","FecEntrada": "01/01/2014 09:08:03","TipRegistro": "S","Message": null,"ConRegistro": 50, "CodProducto" : ""}',		  
+          data: JSON.stringify(datosenta),			  
           success: function(data) // Variable data contains the data we get from serverside
           {   
-		      $('#datoscontador').empty();
-              Mostrarcontador(data,cdv);	
-			  alert("Usuario anonimo contado");
+		      conteventotal=(data.ConEntradasServicio-data.ConSalidasServicio);
+			  if(conteventotal<0){
+			      conteventotal=0;
+			  }
+			  if(conteventotal<=capservicio){
+				  $('.cont').empty();
+				  updatecounter(data.ConEntradasServicio,data.ConSalidasServicio);
+				  alert("Usuario anónimo contado");
+			  }
+			  if(conteventotal>capservicio){
+			      capacidadevento(capservicio);
+			  }	
           },
 		  error: function(data){
-		       alert("Error en la conexion");
+		       alert("Error de conexion al servidor");
 		  }
       });
+}
+function egresarusuarios(){
+    var fecha = new Date();
+    var fechaactual=(fecha.getMonth()+1)+'/'+fecha.getDate()+'/'+fecha.getFullYear()+' '+fecha.getHours()+':'+fecha.getMinutes()+':'+fecha.getSeconds();
+    var datosenta=
+	{
+	   "NitProveedor":nitproveedor,
+	   "CedConsumidor":713999,
+	   "CodServicio":tiposervice,
+	   "FecEntrada":fechaactual,
+	   "TipRegistro":"S",
+	   "Message":null,
+	   "ConRegistro":1,
+	   "CodProducto":""
+	}
+   var url='http://181.48.24.156:8183/Servicios/api/Registro/Add';	
+   $.ajax({ // ajax call starts
+          url: url, // JQuery loads serverside
+		  type:"POST",
+		  headers: { 
+				 Accept : "text/plain; charset=utf-8",
+				"Content-Type": "text/plain; charset=utf-8"
+          },
+          dataType: "json",
+		  async: false,
+          crossDomain: true,
+          data: JSON.stringify(datosenta),			  
+          success: function(data) // Variable data contains the data we get from serverside
+          {   
+		      conteventotal=(data.ConEntradasServicio-data.ConSalidasServicio);
+			  if(conteventotal<0){
+			      conteventotal=0;
+			  }
+			  if(conteventotal<=capservicio){
+				  $('.cont').empty();
+				  updatecounter(data.ConEntradasServicio,data.ConSalidasServicio);
+				  alert("Usuario descontado");
+			  }
+			  if(conteventotal>capservicio){
+			      capacidadevento(capservicio);
+			  }	
+          },
+		  error: function(data){
+		       alert("Error de conexion al servidor");
+		  }
+      });
+}
+function capacidadevento(capev){
+    alert("Se llegó al límite de la capacidad del servicio de "+capev);    
 }
